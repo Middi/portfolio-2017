3cb0d5241aa4ce4f62c6cd425682da54cc8e1aa5
“Richard Middleton” <Richard@richardmiddleton.me>
2017-08-07 22:54:28 +0200

    switched arrows for slider

--- js/main.js
+++ js/main.js
@@ -7,14 +7,14 @@ $(document).ready(function () {
     var sliderpos = 0;
     
     $('.left').click(function () {
-        sliderpos += -140;
+        sliderpos += 140;
         $('.slides').css({
             'margin-left' : '' + sliderpos + 'px'
         });
     });
 
     $('.right').click(function () {
-        sliderpos += +140;
+        sliderpos -= 140;
         $('.slides').css({
             'margin-left' : '' + sliderpos + 'px'
         });
