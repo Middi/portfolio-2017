d1279c2a7ff68d2332d9b542d9f2f8f0cf14c419
“Richard Middleton” <Richard@richardmiddleton.me>
2017-08-07 23:12:36 +0200

    chevron colors

--- js/main.js
+++ js/main.js
@@ -11,6 +11,7 @@ $(document).ready(function () {
         $('.slides').css({
             'margin-left' : '' + sliderpos + 'px'
         });
+        pos();
     });
 
     $('.right').click(function () {
@@ -18,7 +19,43 @@ $(document).ready(function () {
         $('.slides').css({
             'margin-left' : '' + sliderpos + 'px'
         });
+        pos();
     });
+
+
+
+
+    function pos(){
+        if(sliderpos >= 0) {
+            $('.fa-chevron-left').css({
+                'opacity' : '0.3'
+            });
+            $('.fa-chevron-right').css({
+                'opacity' : '1'
+            });
+            console.log(sliderpos);
+        }
+        else if(sliderpos <= -980){
+            $('.fa-chevron-right').css({
+                'opacity' : '0.3'
+            });
+            $('.fa-chevron-left').css({
+                'opacity' : '1'
+            });
+            console.log(sliderpos);
+        }
+        else {
+            $('.fa-chevron-left').css({
+                'opacity' : '1'
+            });
+            $('.fa-chevron-right').css({
+                'opacity' : '1'
+            });
+            console.log(sliderpos);
+        }
+    }
+
+    pos();
 
 
 });
