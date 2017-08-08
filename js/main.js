e946f58c726e88872f009752413d1211f807b2b3
“Richard Middleton” <Richard@richardmiddleton.me>
2017-08-08 22:28:50 +0200

    smooth scrolling

--- js/main.js
+++ js/main.js
@@ -19,6 +19,30 @@ $(document).ready(function () {
   });
 
 
+  // ===========================
+  // Smooth Scrolling
+  // ===========================
+
+    $('a[href*="#"]:not([href="#"])').click(function() {
+        if (location.pathname.replace(/^\//, '') === this.pathname.replace(/^\//, '') && location.hostname === this.hostname) {
+            var target = $(this.hash);
+            target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+
+            if (target.length) {
+                $('html, body').animate({
+                    scrollTop: target.offset().top
+                }, 1000);
+
+                $('#menu').slideUp( "slow", function() {
+                $('#menu').addClass('hidden');
+                
+                });
+                return false;
+            }
+        }
+    });  
+
+
     // ===========================
     // Slider
     // ===========================
